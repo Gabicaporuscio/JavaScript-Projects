0aff637d9abe0e97af108a7c9fde2399f86b225c
Gabriella Caporuscio <91995894+Gabicaporuscio@users.noreply.github.com>
2021-11-13 21:51:16 +0000

    upload commit

---
+++ JavaScript_Projects/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+
+var B = "Welcome!"; //pop up section
+window.alert(B); 
+var A = "Salem said, \"This is a string" + " and I like it\" <br> Sally said, \"yes,it is\""; // using mentions
+document.write(A);
+
+
+
+var cats = "The Caporuscios", sister = "<br>Sally", Brother = "Salem"; //using multiple variables
+var sister = sister.fontcolor("green") // assigning sister variable with green color
+document.write(sister + " is not green"); //displaying sister variable
+
+
+var blues = "<br> Salem"; 
+var blues = blues.fontcolor("blue"); //assign salem to blue color
+document.write(blues + " is not blue."); //displaying salem blue and concatenate string
+
+
+
+document.write("<br>Cats have " + (2+2) + " paws") // expression and concatenate string
