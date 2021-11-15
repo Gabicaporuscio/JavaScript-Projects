67971920b02ce8203af080af2a2d1019917e6dd7
Gabriella Caporuscio <91995894+Gabicaporuscio@users.noreply.github.com>
2021-11-15 20:58:27 +0000

    commit

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function my_function () {
+    var B = "Well done!";
+    document.getElementById("Well done").innerHTML = B
+}
+
+function Princess() {
+    var disney = "You are";
+    disney += " Cinderella!";
+    document.getElementById("disney").innerHTML = disney;
+}
