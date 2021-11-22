c6af9ecd0b05f295ccdecf7a88d69663997c1481
Gabriella Caporuscio <91995894+Gabicaporuscio@users.noreply.github.com>
2021-11-22 20:19:52 +0000

    Comparisons_coercion_commit

---
+++ JavaScript_Projects/Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,51 @@
+document.write(typeof "word" + 6); //string + number
+
+document.write("<br>");
+
+function functionp() {
+    document.getElementById("test").innerHTML = isNaN('string'); //not a number true
+}
+
+document.write("<br>");
+
+document.write(-3E309); // negative infinite
+
+document.write("<br>");
+
+document.write(5>1); //true statement
+
+document.write("<br>");
+
+console.log(2 + 2); // displaing in console
+
+document.write("<br>");
+
+document.write("10" + 5); //value 105 
+
+document.write("<br>");
+
+document.write(5 + 5 == 10); //true compararision
+
+document.write("<br>");
+
+x = 27
+y = 27
+document.write(x === y);  // same value true
+document.write("<br>");                    
+a = 6
+b = 7
+document.write (a === b); // different value false
+
+document.write("<br>");
+
+document.write( 6 > 5 && 7 > 4); // AND true
+
+document.write("<br>");
+
+document.write (5 < 2 || 10 < 5); // OR false
+
+document.write("<br>");
+
+function not_function() {
+   document.getElementById("Not").innerHTML = ! (5>10); // 5 is NOT bigger than 10,so it is true
+}
